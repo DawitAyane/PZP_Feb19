d749c38d2c811740017b01a582464d2d7a0f45db
Alexander <alexander@boeckle.de>
2019-02-04 17:03:29 +0100

    Tag 1 Nachmittag

--- Mario/src/Main.java
+++ Mario/src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("                        ##");
+        System.out.println("                       ###");
+        System.out.println("     ( )              ####");
+        System.out.println("      |              #####");
+        System.out.println("    -----           ######");
+        System.out.println("      |            #######");
+        System.out.print("      |   ");
+        System.err.print("_____" );
+        System.out.println("   ########");
+        System.out.print("     / \\  ");
+        System.err.print(" | |   ");
+        System.out.println("#########");
+        System.out.println("--------------------------");
+        System.out.println("");
     }
 }
 
